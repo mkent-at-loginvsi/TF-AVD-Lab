9939be496b59784c2a466538a86e904a70ab4f8a
Michael Kent <m.kent@loginvsi.com>
2023-07-11 15:07:59 -0700

    initial tf files for Pooled Desktop

---
+++ src/Config-AVD-Pooled-Desktop/variables.tf
@@ -0,0 +1,57 @@
+variable "resource_group_location" {
+  default     = "australiaeast"
+  description = "Location of the resource group - Australia East"
+}
+
+variable "rg_name" {
+  type        = string
+  default     = "AE-DEV-AVD-01-PO-D-RG"
+  description = "Name of the Resource group in which to deploy service objects"
+}
+
+variable "workspace" {
+  type        = string
+  description = "Name of the Azure Virtual Desktop workspace"
+  default     = "AE-DEV-AVD-01-WS"
+}
+
+variable "hostpool" {
+  type        = string
+  description = "Name of the Azure Virtual Desktop host pool"
+  default     = "AE-DEV-AVD-01-PO-D-HP"
+}
+
+variable "app_group_name" {
+  description = "Name of the Azure Virtual Desktop application group"
+  type        = string
+  default     = "AE-DEV-AVD-01-DAG"
+}
+
+variable "rfc3339" {
+  type        = string
+  default     = "2023-05-20T12:43:13Z"  #Update this value with a future date
+  description = "Registration token expiration"
+}
+
+variable "prefix" {
+  type        = string
+  default     = "AE-DEV-AVD-01-HP-"
+  description = "Prefix of the name of the AVD machine(s)"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {
+    Environment = "Dev"
+    Department  = "IT"
+    Location = "AustraliaEast"
+    ServiceClass = "DEV"
+    Workload = "Host Pool 01"
+  }
+}
+
+data "azuread_client_config" "AzureAD" {}
+
+data "azuread_group" "AVDGroup" {
+  display_name     = "Win365-Users"  
+}
